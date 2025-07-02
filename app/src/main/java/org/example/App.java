58bff4b06395b9b0ff8f473b0698a5a383a6c4d4
eomsarang <rosy.sarang.eom@gmail.com>
2025-07-02 15:11:58 +0900

    3단계: 회원관리CRUD, 메서드 활용

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,46 +4,80 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    class User {
-      String name;
-      String email;
-      String password;
-    }
+  static User[] users = new User[10000]; // 레퍼런스 배열
+  static int len = 0;
+  static InputStream stdin = System.in;
+  static Scanner keyboard = new Scanner(stdin);
 
+  public static void main(String[] args) {
     System.out.println("[HomeApp] 애플리케이션");
 
-    InputStream stdin = System.in;
-    Scanner keyboard = new Scanner(stdin);
+    printMenu();
 
-    User[] users = new User[10000]; // 레퍼런스 배열
-    int len = 0;
+    loop:
+    while (true) {
+      String input = prompt("메뉴>");
 
-    while (len < users.length) {
-      users[len] = new User(); // 사용자 정보를 담을 인스턴스 생성하고 인스턴스 주소를 배열에 저장
-
-      System.out.print("이름? ");
-      users[len].name = keyboard.nextLine();
-
-      System.out.print("이메일? ");
-      users[len].email = keyboard.nextLine();
-
-      System.out.print("암호? ");
-      users[len].password = keyboard.nextLine();
-
-      len++;
-
-      System.out.print("계속하시겠습니까? (y/N): ");
-      String response = keyboard.nextLine();
-      if (!response.equalsIgnoreCase("y")) {
-        break;
+      switch (input) {
+        case "1":
+          handleMemberCreate();
+          break;
+        case "2":
+          handleMemberList();
+          break;
+        case "6":
+          break loop;
+        case "menu":
+          printMenu();
+        default:
+          handleError();
       }
     }
 
+    keyboard.close();
+  }
+
+  static String prompt(String title) {
+    System.out.print(title + " ");
+    return keyboard.nextLine().trim();
+  }
+
+  static void printMenu() {
+    System.out.println("메뉴: ");
+    System.out.println("  1. 입력");
+    System.out.println("  2. 목록");
+    System.out.println("  3. 조회");
+    System.out.println("  4. 변경");
+    System.out.println("  5. 삭제");
+    System.out.println("  6. 종료");
+  }
+
+  static void handleMemberCreate() {
+    users[len] = new User(); // 사용자 정보를 담을 인스턴스 생성하고 인스턴스 주소를 배열에 저장
+
+    users[len].name = prompt("이름?");
+    users[len].email = prompt("이메일?");
+    users[len].password = prompt("암호?");
+
+    len++;
+
+    System.out.println("입력되었습니다.");
+  }
+
+  static void handleMemberList() {
     for (int i = 0; i < len; i++) {
       System.out.printf(
           "이름: %s, 이메일: %s, 암호: %s\n", users[i].name, users[i].email, users[i].password);
     }
-    keyboard.close();
+  }
+
+  static void handleError() {
+    System.out.println("메뉴 번호가 유효하지 않습니다.");
+  }
+
+  static class User {
+    String name;
+    String email;
+    String password;
   }
 }
