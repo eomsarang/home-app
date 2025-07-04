76c8046f8a66029e965e568681e97f9ebeb18ea1
eomsarang <rosy.sarang.eom@gmail.com>
2025-07-04 14:53:07 +0900

    클래스 활용: 회원관리 메서드를 클래스로 분리

--- README.md
+++ README.md
@@ -1,14 +1,27 @@
 # home-app
 
-## 1단계: Gradle 도입 전
+## 프로젝트 준비
+
+### v1.0: Gradle 도입 전
 
 - src 파일을 별도 디렉토리로 분리
 - MANIFEST.MF 파일 추가
 
-## 2단계: Gradle 도입 후
+### v2.0: Gradle 도입 후
 
 - Gradle을 사용하여 프로젝트 디렉토리를 구성
 
-## 3단계: Standalone I
+## Standalone 애플리케이션
+
+## v3.0: CRUD 구현
 
 - 회원관리 CRUD
+
+## v3.1: 클래스 활용
+
+- 회원관리 관련 메서드를 클래스로 분리
+- 이론:
+    - SOLID
+        - SRP(Single Responsibility Principle)
+    - GRASP(General Responsibility Assignment Software Patterns)
+        - High Cohesion
--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,39 +4,13 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 public class App {
-  static User[] users = new User[10000]; // 레퍼런스 배열
-  static int len = 0;
   static InputStream stdin = System.in;
   static Scanner keyboard = new Scanner(stdin);
-
-  static void init() {
-    // 예제 데이터를 미리 만들어 배열에 저장한다.
-    User user = new User();
-    user.name = "user1";
-    user.email = "user1@test.com";
-    user.password = "1111";
-    users[len] = user;
-    len++;
-
-    user = new User();
-    user.name = "user2";
-    user.email = "user2@test.com";
-    user.password = "2222";
-    users[len] = user;
-    len++;
-
-    user = new User();
-    user.name = "user3";
-    user.email = "user3@test.com";
-    user.password = "3333";
-    users[len] = user;
-    len++;
-  }
 
   public static void main(String[] args) {
     System.out.println("[HomeApp] 애플리케이션");
 
-    init();
+    MemberHandler.init();
 
     printMenu();
 
@@ -46,19 +20,19 @@ public class App {
 
       switch (input) {
         case "1":
-          handleMemberCreate();
+          MemberHandler.handleMemberCreate();
           break;
         case "2":
-          handleMemberList();
+          MemberHandler.handleMemberList();
           break;
         case "3":
-          handleMemberRead();
+          MemberHandler.handleMemberRead();
           break;
         case "4":
-          handleMemberUpdate();
+          MemberHandler.handleMemberUpdate();
           break;
         case "5":
-          handleMemberDelete();
+          MemberHandler.handleMemberDelete();
           break;
         case "6":
           break loop;
@@ -87,87 +61,8 @@ public class App {
     System.out.println("  6. 종료");
   }
 
-  static void handleMemberCreate() {
-    users[len] = new User(); // 사용자 정보를 담을 인스턴스 생성하고 인스턴스 주소를 배열에 저장
-
-    users[len].name = prompt("이름?");
-    users[len].email = prompt("이메일?");
-    users[len].password = prompt("암호?");
-
-    len++;
-
-    System.out.println("입력되었습니다.");
-  }
-
-  static void handleMemberList() {
-    for (int i = 0; i < len; i++) {
-      if (users[i].email.equals("")) {
-        continue;
-      }
-      System.out.printf(
-          "이름: %s, 이메일: %s, 암호: %s\n", users[i].name, users[i].email, users[i].password);
-    }
-  }
-
   static void handleError() {
     System.out.println("메뉴 번호가 유효하지 않습니다.");
-  }
-
-  static void handleMemberRead() {
-
-    String email = prompt("이메일?");
-    for (int i = 0; i < len; i++) {
-      User user = users[i];
-      // user레퍼런스가 가리키는 객체의 email 변수 값이 사용자가 입력한 email 변수 값과 같은지 비교해야 한다.
-      if (user.email.equals(email)) {
-        System.out.printf("이름: %s\n", user.name);
-        System.out.printf("이메일: %s\n", user.email);
-        System.out.printf("암호: %s\n", user.password);
-        return;
-      }
-    }
-    System.out.println("해당 회원이 존재하지 않습니다.");
-  }
-
-  static void handleMemberUpdate() {
-    String email = prompt("이메일?");
-    for (int i = 0; i < len; i++) {
-      User user = users[i];
-      // user레퍼런스가 가리키는 객체의 email 변수 값이 사용자가 입력한 email 변수 값과 같은지 비교해야 한다.
-      if (user.email.equals(email)) {
-        String name = prompt("이름(%s):", user.name);
-        String password = prompt("암호(%s):", user.password);
-        String response = prompt("정말 변경하시겠습니까? (Y/n)");
-        if (response.equalsIgnoreCase("Y") || response.equals("")) {
-          user.name = name;
-          user.password = password;
-          System.out.println("변경했습니다.");
-        } else {
-          System.out.println("변경 취소했습니다.");
-        }
-        return;
-      }
-    }
-    System.out.println("해당 회원이 존재하지 않습니다.");
-  }
-
-  static void handleMemberDelete() {
-    String email = prompt("이메일?");
-    for (int i = 0; i < len; i++) {
-      User user = users[i];
-      // user레퍼런스가 가리키는 객체의 email 변수 값이 사용자가 입력한 email 변수 값과 같은지 비교해야 한다.
-      if (user.email.equals(email)) {
-        String response = prompt("정말 삭제하시겠습니까? (Y/n)");
-        if (response.equalsIgnoreCase("Y") || response.equals("")) {
-          user.email = "";
-          System.out.println("삭제했습니다.");
-        } else {
-          System.out.println("삭제 취소했습니다.");
-        }
-        return;
-      }
-    }
-    System.out.println("해당 회원이 존재하지 않습니다.");
   }
 
   // 클래스?
